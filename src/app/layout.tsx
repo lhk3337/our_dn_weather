83f3364e27dd61449f74f8bfa95b9fe3f993dcd6
lhk3337 <lhk3337@naver.com>
2023-02-24 16:53:38 +0900

    [Init] nextjs Init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
-import './globals.css'
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+      <head>
+        <title>Next.js 13</title>
+      </head>
+
+      <body>
+        <nav>Next.js 13</nav>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
