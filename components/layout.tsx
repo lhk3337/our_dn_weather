4c434e893801e0993c18047fb5d5dd9ab778da00
lhk3337 <lhk3337@naver.com>
2023-03-09 17:59:00 +0900

    [FEAT] favicon 설정하기

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,9 +3,6 @@ import Nav from "./nav";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Head>
-        <title>우리 동네 날씨</title>
-      </Head>
       <Nav />
       <div>{children}</div>
     </>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,22 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
+import Head from "next/head";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig
-      value={{
-        fetcher: (url: string) => fetch(url).then((r) => r.json()),
-      }}
-    >
-      <Component {...pageProps} />
-    </SWRConfig>
+    <>
+      <Head>
+        <title>우리 동네 날씨</title>
+        <link rel="icon" type="image/svg" href="/favicon.svg" />
+      </Head>
+      <SWRConfig
+        value={{
+          fetcher: (url: string) => fetch(url).then((r) => r.json()),
+        }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
+    </>
   );
 }
 export default MyApp;
