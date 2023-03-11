7339074e976405fb333fb3270f475ec5d90fbd37
lhk3337 <lhk3337@naver.com>
2023-03-11 17:50:51 +0900

    [refactor] 용도에 따른 libs 폴더 분할

--- libs/server/apiUrl.ts
+++ libs/server/apiUrl.ts
@@ -1,5 +1,5 @@
-const apiUrl = async (x: number, y: number) => {
-  const url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"; /*URL*/
+const apiUrl = async (x: number, y: number, islowHightemper: boolean) => {
+  const url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"; /*단기예보조회*/
   // function nowHour() {
   //   // 초단기 예보 날씨 api 데이터가 정시 기준으로 45분 이후에 새로 업데이트 함
   //   // 45분전이면 1시간 전 시간을, 45분보다 크면 현재 시간을 가져온다.
@@ -46,9 +46,22 @@ const apiUrl = async (x: number, y: number) => {
       return `${year}${month}${day}`;
     }
   };
+  if (islowHightemper) {
+    let queryParams = "?" + encodeURIComponent("serviceKey") + "=" + process.env.SERVICE_KEY;
+    queryParams += "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /**/
+    queryParams += "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("158"); /**/
+    queryParams += "&" + encodeURIComponent("dataType") + "=" + encodeURIComponent("JSON"); /**/
+    queryParams +=
+      "&" + encodeURIComponent("base_date") + "=" + encodeURIComponent(getToday()); /*오늘 날짜를 yyyymmdd로 표현 */
+    queryParams += "&" + encodeURIComponent("base_time") + "=" + encodeURIComponent("0200"); /* 예보 발표 시간  */
+    queryParams += "&" + encodeURIComponent("nx") + "=" + encodeURIComponent(x); /**/
+    queryParams += "&" + encodeURIComponent("ny") + "=" + encodeURIComponent(y); /**/
+    return (await fetch(url + queryParams)).json();
+  } else {
+  }
   let queryParams = "?" + encodeURIComponent("serviceKey") + "=" + process.env.SERVICE_KEY;
   queryParams += "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /**/
-  queryParams += "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("1000"); /**/
+  queryParams += "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("115"); /**/
   queryParams += "&" + encodeURIComponent("dataType") + "=" + encodeURIComponent("JSON"); /**/
   queryParams +=
     "&" + encodeURIComponent("base_date") + "=" + encodeURIComponent(getToday()); /*오늘 날짜를 yyyymmdd로 표현 */
